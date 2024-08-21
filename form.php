ec4735f90c1c047c9cfccd9fb863dbdf5b3fe64a
lylamthu <lylamthu2612@gmail.com>
2024-08-21 21:07:57 +0700

    first commit

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Form</title>
+</head>
+<body>
+
+    <h2>Simple PHP Form</h2>
+
+    <form method="post" action="">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br><br>
+        
+        <label for="age">Age:</label><br>
+        <input type="text" id="age" name="age"><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
